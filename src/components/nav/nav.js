793273aa0bf081abf549e69abe1837fde35b0ba0
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-09-06 13:55:12 -0500

    Nav bar designed and githum repo links added

---
+++ src/components/nav/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './nav.css';
+
+const Nav = () => (
+  <nav className="navbar mb-1 navigation">
+    <div className="navbar-header">
+      <h2 className="navbar-brand" href="https://github.com/suresh7127/Clicky-Game2" target="_blank">Clicky-Clicky!!</h2>
+    </div>
+    <ul className="nav navbar-nav">
+      <h2 href="https://github.com/suresh7127/Clicky-Game2" target="_blank">GitHub Repo</h2>
+    </ul>
+  </nav>
+);
+
+export default Nav;
