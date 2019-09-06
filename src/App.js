fb11e654500603be927c890113db6a5a3d61b56a
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-09-06 18:29:07 -0500

    import file path updated to fix the module error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import chars from './chars.json';
-import Wrapper from './components/wrapper';
+import Wrapper from './components/wrapper/wrapper';
 import Nav from './components/nav/nav';
-import Header from './components/header';
-import Scoreboard from './components/scoreboard';
-import ImgCard from './components/imgCard';
-import GameOver from './components/gameover';
+import Header from './components/header/header';
+import Scoreboard from './components/scoreboard/scoreboard';
+import ImgCard from './components/imgCard/imgCard';
+import GameOver from './components/gameover/gameover';
 
 function randomChars(array) {
   for (let i = array.length - 1; i > 0; i--) {
