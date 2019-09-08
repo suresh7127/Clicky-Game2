1451b75f5db06b2b7ba594dfea813c4ac444e6b7
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-09-07 23:42:44 -0500

    github.io link removed

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -5,7 +5,7 @@ const Nav = () => (
   <nav className="navbar mb-1 navigation">
     <div className="navbar-header">
       <h2 className="navbar-brand">
-        <a href="https://suresh7127.github.io/Clicky-Game2/">Clicky-Clicky!!</a>
+        {/* <a href="https://suresh7127.github.io/Clicky-Game2/">Clicky-Clicky!!</a> */}
       </h2>
     </div>
     <ul className="nav navbar-nav">
