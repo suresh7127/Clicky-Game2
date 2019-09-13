dccb5440b201e50cd9827305d18d3f4a281b44ea
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-09-12 21:32:35 -0500

    Updates

---
+++ precache-manifest.eb65607ac32ae0e983ce33c02ac86d40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d685b81f2022fe70db40e2d4efab82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66eeba147eeb3018a036",
+    "url": "/static/css/main.ac322f8c.chunk.css"
+  },
+  {
+    "revision": "4e425f0932dbc2f21e9c",
+    "url": "/static/js/2.3beea9df.chunk.js"
+  },
+  {
+    "revision": "66eeba147eeb3018a036",
+    "url": "/static/js/main.bd0ba78f.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  }
+]);
